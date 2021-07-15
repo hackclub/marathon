166a5d654f6ab9c84d188142ebe16395dcf5eca0
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-07-15 19:18:38 +0800

    Early Work on CLI

---
+++ cli/index.js
@@ -0,0 +1,67 @@
+const readline = require("readline");
+const gradient = require("gradient-string");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+
+function blankScreen() {
+  // Function that we'll use to show simply a blank screen
+  readline.cursorTo(process.stdout, 0, 0); // Moves the user cursor to 0,0
+  readline.clearScreenDown(process.stdout); // Clears the screen and make it blank
+}
+
+blankScreen();
+
+async function main() {
+  console.log(
+    chalk.green(
+      [
+        "          /\\ ",
+        "         /**\\ ",
+        "        /****\\   /\\ ",
+        "       /      \\ /**\\ ",
+        "      /  /\\    /    \\        /\\    /\\  /\\      /\\            /\\/\\/\\  /\\ ",
+        "     /  /  \\  /      \\      /  \\/\\/  \\/  \\  /\\/  \\/\\  /\\  /\\/ / /  \\/  \\ ",
+        "    /  /    \\/ /\\     \\    /    \\ \\  /    \\/ /   /  \\/  \\/  \\  /    \\   \\ ",
+        "   /  /      \\/  \\/\\   \\  /      \\    /   /    \\ ",
+        "__/__/_______/___/__\\___\\__________________________________________________",
+      ].join("\n")
+    )
+  );
+
+  console.log(
+    chalk.green(
+      `  
+████████ ██   ██ ███████     ███    ███  █████  ██████   █████  ████████ ██   ██  ██████  ███    ██ 
+   ██    ██   ██ ██          ████  ████ ██   ██ ██   ██ ██   ██    ██    ██   ██ ██    ██ ████   ██ 
+   ██    ███████ █████       ██ ████ ██ ███████ ██████  ███████    ██    ███████ ██    ██ ██ ██  ██ 
+   ██    ██   ██ ██          ██  ██  ██ ██   ██ ██   ██ ██   ██    ██    ██   ██ ██    ██ ██  ██ ██ 
+   ██    ██   ██ ███████     ██      ██ ██   ██ ██   ██ ██   ██    ██    ██   ██  ██████  ██   ████ 
+`
+    )
+  );
+  const { id } = await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "id",
+        message: "What is your participant ID (provided by email)?",
+      },
+    ])
+    .then((answers) => {
+      return answers;
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log(
+          "This terminal does not support this tool, please try another"
+        );
+        throw error;
+      } else {
+        console.log("Error!");
+        throw error;
+      }
+    });
+  let startResponse = await fetch().then(r => r.json()); 
+}
+
+main();
