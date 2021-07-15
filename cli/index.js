f4b6c9bf077b911f4d8624f9e3599a6c645c86c9
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-07-15 20:01:05 +0800

    Add Index to Start

--- cli/index.js
+++ cli/index.js
@@ -3,11 +3,31 @@ const gradient = require("gradient-string");
 const chalk = require("chalk");
 const inquirer = require("inquirer");
 const fetch = require("node-fetch");
+const figlet = require("figlet");
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 function blankScreen() {
   // Function that we'll use to show simply a blank screen
   readline.cursorTo(process.stdout, 0, 0); // Moves the user cursor to 0,0
   readline.clearScreenDown(process.stdout); // Clears the screen and make it blank
+}
+
+async function showQuestion(index) {
+  await figlet(
+    `Stage ${index}`,
+    {
+      font: "ANSI Regular",
+    },
+    function (err, data) {
+      if (err) {
+        console.log(`Stage ${index}`);
+      }
+      console.log(chalk.blue(data));
+    }
+  );
 }
 
 blankScreen();
@@ -62,11 +82,26 @@ async function main() {
         throw error;
       }
     });
-  let startResponse = await fetch(`https://marathon-api.hackclub.dev/start?id=${id}`).then(r => r.json()); 
-  if(startResponse.error){
-    console.log(chalk.red('\nError! ' + startResponse.error + '\n'));
+  let startResponse = await fetch(
+    `https://marathon-api.hackclub.dev/start?id=${id}`
+  ).then((r) => r.json());
+  if (startResponse.error) {
+    console.log(chalk.red("\nError! " + startResponse.error + "\n"));
     return;
   }
+  if (startResponse.starting) {
+    console.log(
+      chalk.blue(
+        "\nHey " +
+          startResponse.user["Participant Name"] +
+          "! Loading the first stage for you...\n"
+      )
+    );
+  }
+  await sleep(2000);
+  blankScreen();
+  console.log(startResponse)
+  await showQuestion(startResponse.index);
 }
 
 main();
